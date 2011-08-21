f2bbd3ca80565cecc651b1962d5764d441cf743b
spencer <sepmein@gmail.com>
2011-08-21 15:30:58 +0800

    fixed some bugs

--- js/nolocalstorage.js
+++ js/nolocalstorage.js
@@ -2,75 +2,77 @@
  *使用 prototype原型继承，实时刷新article值
  */
 function Localarticle(index, title, content) {
-	this.index = index;
-	this.title = title;
-	this.content = content;
+    this.index = index;
+    this.title = title;
+    this.content = content;
 }
 
 Localarticle.prototype = new Article();
 Localarticle.prototype.refresh = function(i) {
-	this.index = i;
-	this.title = alldata[i].chapter;
-	this.content = alldata[i].content;
-}
+    this.index = i;
+    this.title = alldata[i].chapter;
+    this.content = alldata[i].content;
+};
 var thisarticle = new Localarticle(0, alldata[0].chapter, alldata[0].content);
 
 /*setup quick view section*/
+
 function setupQuickView() {
-	var articlecontent = "<h1>" + thisarticle.index + ".&nbsp" + thisarticle.title + "</h1>"+ thisarticle.content;
-	var articleElement = document.getElementsByTagName('article')[0];
-	articleElement.innerHTML = articlecontent;
+    var articlecontent = "<h1>" + thisarticle.index + ".&nbsp" + thisarticle.title + "</h1>" + thisarticle.content;
+    var articleElement = document.getElementsByTagName('article')[0];
+    articleElement.innerHTML = articlecontent;
 }
 
 setupQuickView();
 
 function initProgressBar() {
-	var para = document.createElement('p');
-	$(para).insertAfter($('#we>aside>h3'));
-	var readingprogress = document.createElement('progress');
-	$(readingprogress).appendTo($('#we aside p'));
-	readingprogress.max = 59;
-	readingprogress.value = 0;
+    var para = document.createElement('p');
+    $(para).insertAfter($('#we>aside>h3'));
+    var readingprogress = document.createElement('progress');
+    $(readingprogress).appendTo($('#we aside p'));
+    readingprogress.max = 59;
+    readingprogress.value = 0;
 }
 
 function setProgressBarValue(value) {
-	document.getElementsByTagName('progress')[0].value = value;
+    document.getElementsByTagName('progress')[0].value = value;
 }
 
-if(Boolean(document.createElement('progress').value)) {
-	initProgressBar();
+if (Boolean(document.createElement('progress').value)) {
+    initProgressBar();
 } else {
-	/*
-	 * fallback solutions here.
-	 * use progresspolyfill
-	 */
-	yepnope({
-		load : ['css/progresspolyfill.css', 'js/mylibs/progresspolyfill.min.js'],
-		complete : function() {initProgressBar();
-		}
-	})
-}
-/*两个按钮的代码：切换，改变progress*/
+/*
+     * fallback solutions here.
+     * use progresspolyfill
+     */
+    yepnope({
+        load: ['css/progresspolyfill.css', 'js/mylibs/progresspolyfill.min.js'],
+        complete: function() {
+            initProgressBar();
+        }
+    });
+} /*两个按钮的代码：切换，改变progress*/
+
 function toPrevious() {
-	var chapter = thisarticle.index;
-	if(chapter != 0) {
-		thisarticle.refresh(chapter - 1);
-		setupQuickView();
-		setProgressBarValue(thisarticle.index);
-		$('li').removeClass('liselected');
-		$($('li')[thisarticle.index]).addClass('liselected');
-	}
+    var chapter = thisarticle.index;
+    if (chapter !== 0) {
+        thisarticle.refresh(chapter - 1);
+        setupQuickView();
+        setProgressBarValue(thisarticle.index);
+        $('li').removeClass('liselected');
+        $($('li')[thisarticle.index]).addClass('liselected');
+    }
 }
 
 function toNext() {
-	var chapter = thisarticle.index;
-	if(chapter != 59) {
-		thisarticle.refresh(chapter + 1);
-		setupQuickView();
-		setProgressBarValue(thisarticle.index);
-		$('li').removeClass('liselected');
-		$($('li')[thisarticle.index]).addClass('liselected');
-	}
+    var chapter = thisarticle.index;
+    if (chapter !== 59) {
+        thisarticle.refresh(chapter + 1);
+        setupQuickView();
+        setProgressBarValue(thisarticle.index);
+        $('li').removeClass('liselected');
+        $($('li')[thisarticle.index]).addClass('liselected');
+    }
 }
 
 var btn = document.getElementsByTagName('button');
@@ -78,30 +80,31 @@ EventUtil.addHandler(btn[0], 'click', toPrevious);
 EventUtil.addHandler(btn[1], 'click', toNext);
 
 /*自动生成章节目录*/
+
 function initNav() {
-	var nav = document.createElement('nav');
-	var ul = document.createElement('ul');
-	var lis = [];
-	for(var i = 0; i < 60; i++) {
-		lis[i] = document.createElement('li');
-		lis[i].innerHTML = i + '.' + alldata[i].chapter;
-		ul.appendChild(lis[i]);
-	}
-	nav.appendChild(ul);
-	$('#we>aside')[0].appendChild(nav);
+    var nav = document.createElement('nav');
+    var ul = document.createElement('ul');
+    var lis = [];
+    for (var i = 0; i < 60; i++) {
+        lis[i] = document.createElement('li');
+        lis[i].innerHTML = i + '.' + alldata[i].chapter;
+        ul.appendChild(lis[i]);
+    }
+    nav.appendChild(ul);
+    $('#we>aside')[0].appendChild(nav);
 }
 
 initNav();
 
 function toChapter() {
-	$('li').removeClass('liselected');
-	var chapter = parseInt(this.innerText);
-	thisarticle.refresh(chapter);
-	setupQuickView();
-	setProgressBarValue(chapter);
-	$(this).addClass('liselected');
+    $('li').removeClass('liselected');
+    var chapter = parseInt(this.innerText);
+    thisarticle.refresh(chapter);
+    setupQuickView();
+    setProgressBarValue(chapter);
+    $(this).addClass('liselected');
 }
 
 $().ready(function() {
-	$('li').bind('click', toChapter);
+    $('li').bind('click', toChapter);
 })
--- js/script.js
+++ js/script.js
@@ -3,51 +3,55 @@
  */
 
 function storaged() {
-	return Modernizr.localstorage && Boolean((localStorage.getItem('直升班——结构')) && Boolean(localStorage.getItem('1')) && Boolean(localStorage.getItem('印象——过渡')));
+    return Modernizr.localstorage && Boolean((localStorage.getItem('直升班——结构')) && Boolean(localStorage.getItem('1')) && Boolean(localStorage.getItem('印象——过渡')));
 }
 
 function Article(index, title, content) {
-	this.index = index;
-	this.title = title;
-	this.content = content;
+    this.index = index;
+    this.title = title;
+    this.content = content;
 }
 
 Article.prototype = {
-	constructor : Article
+    constructor: Article
 };
 
-$(document).ready(function() {toWe();
-});
-function toWe() {
-	$('aside>img').click(function() {
-		$('#wedding').hide('fast', function() {
-			$('#we>div').hide('fast', function() {
-				$('header span').text('/threeyears');
-				$('header h1').click(function() {
-					backFromWe();
-				});
-			});
-			$('#we>section').show('slow');
-			$('#we>aside>nav').fadeIn();
-			$('#we>aside>p').fadeIn();
-			$('#we .vbar').hide();
-		});
-	});
+function backFromWe() {
+    $('#we>section').hide('slow', function() {
+        $('#we>aside>nav').fadeOut();
+        $('#we>aside>p').fadeOut('normal', function() {
+            $('#we>div').show('fast', function() {
+                $('#wedding').show('fast');
+                $('header span').text('.com');
+                $('header h1').unbind('click');
+                $('#we .vbar').show();
+            });
+        });
+    });
 }
 
-function backFromWe() {
-	$('#we>section').hide('slow', function() {
-		$('#we>aside>nav').fadeOut();
-		$('#we>aside>p').fadeOut('normal', function() {
-			$('#we>div').show('fast', function() {
-				$('#wedding').show('fast');
-				$('header span').text('.com');
-				$('header h1').unbind('click');
-				$('#we .vbar').show();
-			});
-		});
-	});
+function toWe() {
+    $('aside>img').click(function() {
+        $('#wedding').hide('fast', function() {
+            $('#we>div').hide('fast', function() {
+                $('header span').text('/threeyears');
+                $('header h1').click(function() {
+                    backFromWe();
+                });
+            });
+            $('#we>section').show('slow');
+            $('#we>aside>nav').fadeIn();
+            $('#we>aside>p').fadeIn();
+            $('#we .vbar').hide();
+        });
+    });
 }
+
+
+$(document).ready(function() {
+    toWe();
+});
+
 
 /*
  https://github.com/Modernizr/Modernizr/issues/308
@@ -57,39 +61,38 @@ function backFromWe() {
  如果modernizr修复了这个bug，用回min版
  */
 yepnope([{
-	test : storaged(),
-	yep : {
-		'local' : 'js/localstorage.js'
-	},
-	nope : {
-		'we' : 'js/we.js'
-	},
-	callback : {
-		'we' : function(url, result, key) {
-			if(Modernizr.localstorage) {
-				yepnope('js/localstorage.js');
-			} else {
-				yepnope('js/nolocalstorage.js');
-			}
-		}
-	}
-}, {
-	test : Modernizr.fontface,
-	yep : 'css/font/Disco_fontface.css'
-}, {
-	test : 1,
-	load : ['http://tjs.sjs.sinajs.cn/t3/platform/js/api/wb.js', 'http://tjs.sjs.sinajs.cn/t3/style/css/common/card.css'],
-	complete : function() {
-		WB.core.load(['connect', 'client', 'widget.base', 'widget.atWhere'], function() {
-			var cfg = {
-				key : '2285514699',
-				xdpath : 'http://kokiya.com/xd.html'
-			};
-			WB.connect.init(cfg);
-			WB.client.init(cfg);
+    test: storaged(),
+    yep: {
+        'local': 'js/localstorage.js'
+    },
+    nope: {
+        'we': 'js/we.js'
+    },
+    callback: {
+        'we': function(url, result, key) {
+            if (Modernizr.localstorage) {
+                yepnope('js/localstorage.js');
+            } else {
+                yepnope('js/nolocalstorage.js');
+            }
+        }
+    }},
+{
+    test: Modernizr.fontface,
+    yep: 'css/font/Disco_fontface.css'},
+{
+    test: 1,
+    load: ['http://tjs.sjs.sinajs.cn/t3/platform/js/api/wb.js', 'http://tjs.sjs.sinajs.cn/t3/style/css/common/card.css'],
+    complete: function() {
+        WB.core.load(['connect', 'client', 'widget.base', 'widget.atWhere'], function() {
+            var cfg = {
+                key: '2285514699',
+                xdpath: 'http://kokiya.com/xd.html'
+            };
+            WB.connect.init(cfg);
+            WB.client.init(cfg);
 
-			//user card widget
-			WB.widget.atWhere.searchAndAt($('#we>aisde>h3')[0]);
-		});
-	}
-}]);
+            //user card widget
+            WB.widget.atWhere.searchAndAt($('#we>aisde>h3')[0]);
+        });
+    }}]);
