bea7e41d43aba3b1e789afbbf61d72935e44fb44
spencer <sepmein@gmail.com>
2012-01-19 23:10:22 +0800

    totally new designed!!

--- js/script.js
+++ js/script.js
@@ -1,98 +1,239 @@
-﻿/* Author: spencer
- to-do:研究modernizr.load 封装代码；改进initRecent，封装进一个对象？
- */
+/* Author:
+	Spencer Zhang
+	sepmein@gmail.com
+	Copy right reserved
+*/
 
-function storaged() {
-    return Modernizr.localstorage && Boolean((localStorage.getItem('直升班——结构')) && Boolean(localStorage.getItem('1')) && Boolean(localStorage.getItem('印象——过渡')));
-}
+// Add an Object method that doesn't  // affect for/in loops 
+Object.defineProperty(
+	Object.prototype,
+	"extend",
+	{
+		enumerable: false,
+		writable: true,
+		configurable: true,
+		value: function (from) {
+			var names = Object.getOwnPropertyNames(from);
+			names.forEach(function (n) {
+				if (n in this) return;
+				var d = Object.getOwnPropertyDescriptor(from, n);
+				Object.defineProperty(this, n, d);
+			});
+		}
+    }
+);
 
-function Article(index, title, content) {
-    this.index = index;
-    this.title = title;
-    this.content = content;
-}
+/*core*/
 
-Article.prototype = {
-    constructor: Article
-};
+var kokiya = Object.create(Object.prototype);
 
-function backFromWe() {
-    $('#we>section').hide('slow', function() {
-        $('#we>aside>nav').fadeOut();
-        $('#we>aside>p').show('normal', function() {
-            $('#we>div').show('fast', function() {
-                $('#wedding').show('fast');
-                $('header span').text('.com');
-                $('header h1').unbind('click');
-                $('#we .vbar').show();
-            });
-        });
-    });
-}
+Object.defineProperties(kokiya, {
 
-function toWe() {
-    $('aside>img').click(function() {
-        $('#wedding').hide('fast', function() {
-            $('#we>div').hide('fast', function() {
-                $('header span').text('/threeyears');
-                $('header h1').click(function() {
-                    backFromWe();
-                });
-            });
-            $('#we>section').show('slow');
-            $('#we>aside>nav').fadeIn();
-            $('#we>aside>p').fadeIn();
-            $('#we .vbar').hide();
-        });
-    });
-}
+	"testResult" : {
+
+		/*
+			测试浏览器的兼容性。返回一个对象。包含一个
+			目前只测试浏览器是否使用html5 localStorage储存过小毛的文。
+			使用testResult动态加载js文件。
+			将来扩展。
+		*/
+
+		
+		value : (function () {
+
+			var result = {};
+			/*&#x6D4B;&#x6D4B;&#x4EE5;&#x524D;&#x6709;&#x7C73;&#x6709;&#x4E0A;&#x8FC7;*/
+			result.storaged = Modernizr.localstorage && Boolean((localStorage.getItem('&#x76F4;&#x5347;&#x73ED;&#x2014;&#x2014;&#x7ED3;&#x6784;')) && Boolean(localStorage.getItem('1')) && Boolean(localStorage.getItem('&#x5370;&#x8C61;&#x2014;&#x2014;&#x8FC7;&#x6E21;')));
+
+			return result;
+
+		})(),
+
+		writable : true,
+		configurable :  true,
+		enumerable :  true
+
+	},
+
+	/*init kokiya*/
+
+	"init" : {
+
+		/*
+			在loadModule函数作用完后，递归执行被加载入kokiya.module数组的模块。
+			成功后在console记录一条消息。
+			目前没有参数。
+		*/
+		
+		value : function () {
+			
+			for (var i = this.module.length - 1; i >= 0; i--) {
+				for (var prop in this.module[i]){
+					this.module[i][prop]();
+					/*for debug*/
+					console.log("Module "+prop+" loaded");
+				}
+			};
 
 
-$(document).ready(function() {
-    toWe();
+		},
+		writable : true,
+		configurable :  true,
+		enumerable :  true
+
+	},
+
+	"module" : {
+		
+
+		/*
+			一个数组，由Spencer臆想出来的模块的组合
+		*/
+
+		value : [],
+		writable : true,
+		configurable : true,
+		enumerable : true
+
+	},
+
+	"loadModule" : {
+		
+		/*
+			加载模块，加载的参数称为“模块”。（模块是由spencer臆想出来的东西）
+			“模块”可以是一个由对象构成的数组，也可以是一个对象。
+			无论是数组还是对象，这个对象都由{”模块名“：”函数体“}构成。
+			所加载的模块自动推送到kokiya.module数组。
+		*/
+
+		value : function (m) {
+			
+			if( Array.isArray(m) ) {
+				for (var i = m.length - 1; i >= 0; i--) {
+					this.module.push(m[i]);
+				};
+			} else {
+				this.module.push(m);
+			}
+
+		},
+		writable : true,
+		configurable : true,
+		enumerable : true
+
+	}
+
 });
 
+kokiya.loadModule(
 
-/*
- https://github.com/Modernizr/Modernizr/issues/308
- Modernizr mini version ie7/8 bug
- take care about that
- 使用标准版Modernizr&yepnope ,fixed
- 如果modernizr修复了这个bug，用回min版
- */
-yepnope([{
-    test: storaged(),
-    yep: {
-        'local': 'js/localstorage.js'
-    },
-    nope: {
-        'we': 'js/we.js'
-    },
-    callback: {
-        'we': function(url, result, key) {
-            if (Modernizr.localstorage) {
-                yepnope('js/localstorage.js');
-            } else {
-                yepnope('js/nolocalstorage.js');
-            }
-        }
-    }},
-{
-    test: Modernizr.fontface,
-    yep: 'css/font/Disco_fontface.css'},
-{
-    test: 1,
-    load: ['http://tjs.sjs.sinajs.cn/t3/platform/js/api/wb.js', 'http://tjs.sjs.sinajs.cn/t3/style/css/common/card.css'],
-    complete: function() {
-        WB.core.load(['connect', 'client', 'widget.base', 'widget.atWhere'], function() {
-            var cfg = {
-                key: '2285514699',
-                xdpath: 'http://kokiya.com/xd.html'
-            };
-            WB.connect.init(cfg);
-            WB.client.init(cfg);
+	[
+		/*&#x4E3A;link&#x589E;&#x52A0;bootstrap popover&#x6548;&#x679C;*/
+		{
+			"popover" : function () {
+				$('footer a').popover();
+			}
+		},
+		{
+			"wrapImage" : function () {  
+  
+				$("img").load(function() {  
+			   		$(this).wrap(function(){  
+			      		return '<span class="image-wrap ' + $(this).attr('class') + '" style="position:relative; display:block; background:url(' + $(this).attr('src') + ') no-repeat center center; width: ' + $(this).width() + 'px; height: ' + $(this).height() + 'px;" />';  
+			    	});  
+			    $(this).css("opacity","0");  
+			  });  
+			  
+			}
+		},
+		{
+			"hideContent" : function () {
 
-            //user card widget
-            WB.widget.atWhere.searchAndAt($('#we>aisde>h3')[0]);
-        });
-    }}]);
+				$(".articleContent").hide();
+
+			}	
+		},
+		{
+			"sliderbox" : function () {
+			
+				var wrapbox = $('<div></div>').addClass('wrapbox');
+				$('#main').after(wrapbox);
+				$('<div></div>').addClass('sliderbox').appendTo(wrapbox);
+
+				function createSubDiv (className){
+					var div=$('<div></div>');
+					div.addClass('subbox'+' '+className);
+					div.html($("#"+className+">div.articleContent").html());
+					div.appendTo($(".sliderbox"));	
+				};
+				createSubDiv("kolumn");
+				createSubDiv("we");
+				createSubDiv("wedding");
+
+
+
+				//get content
+				function showDetail() {
+					
+					var kolumnLeft = '0px',
+						weLeft = '-960px',
+						weddingLeft = '-1920px';
+
+					if ( $('div.wrapbox').css('display') == 'none' ) {
+						
+						// display slider box,change location,change pointer
+						$('div.sliderbox').css('left',setPosition(this));
+						$('div.wrapbox').slideDown();
+
+					} else if($(this).hasClass(checkPosition())) {
+						/*slider已显示，并且是当前tab*/
+						//close slider box
+						//tweek more beautiful effects
+						$('div.wrapbox').slideUp();
+
+					} else /*slider已显示，并且不是当前tab*/ {
+						
+						//change to current location
+						$('div.sliderbox').css('left',setPosition(this));
+					}
+
+					function checkPosition(){
+						var position = $('div.sliderbox').css('left');
+
+						switch (position) {
+							case '0px' : return 'kolumn';
+							case '-960px' : return 'we';
+							case '-1920px' : return 'wedding';
+
+						}
+					
+					
+							
+					}
+
+					function setPosition(object){
+						var currentObject = object;
+						var who = $(currentObject);
+						if (who.hasClass('kolumn')) {
+							return '0px';
+						} else if (who.hasClass('we')) {
+							return '-960px';
+						} else if (who.hasClass('wedding')) {
+							return '-1920px';
+						} else {
+							console.log($(this));
+						}
+ 
+					}
+				
+				}
+
+				$('#main>article').bind('click',showDetail);
+
+			}
+		}
+	]
+
+);
+
+kokiya.init();
